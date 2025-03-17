99a7ac700ab1d39c74b465f5607b7d287dc0e0c5
Dabbcomputers <dabbcomputers@gmail.com>
2025-03-17 09:52:47 -0700

    added links and details

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
-import { ScrollRestoration } from 'react-router-dom'
+import { Link, ScrollRestoration } from 'react-router-dom'
 import CardThree from '@/components/sections/blogs/cardThree'
 import PageTitle from '@/components/sections/pageTitle'
 import Sidebar from '@/components/sections/sidebar/sidebar'
 import Pagination from '@/components/ui/pagination'
 import { blogDataThree } from '@/lib/fackdata/blogDataThree'
 import NewsletterTwo from '@/components/sections/newsletters/newsletterTwo'
+import SectionName from '@/components/ui/sectionName'
+import SectionDescription from '@/components/ui/sectionDescription'
+import SlideUp from '@/lib/animations/slideUp'
+import Title from '@/components/ui/title'
+import { Button } from '@/components/ui/button'
 
 const Blog = () => {
   return (
@@ -25,9 +30,28 @@ const Blog = () => {
                   // blogDataThree.map(({ author, blog_desc, date, id, src, title }) => <CardThree key={id} src={src} title={title} />)
                   
                 }
-                <iframe class="google-container" src="https://docs.google.com/document/d/1WHbWLqe1T1dDS98xzZez_VTrNKLjXVOKYtURpjQ0zqA/preview?usp=sharing&embedded=true" 
+                <div className="">
+                        <SectionName>Documents</SectionName>
+                        <Title size={"3.5xl"} className={"lg:max-w-[410px] mt-2.5"}>Download Documents</Title>
+                        <SectionDescription className={'mt-5'}>Get all the essential details about our programs, policies, schedules, and more in our Parent Handbook. Download it today to stay informed and ensure a smooth experience for you and your child!</SectionDescription>
+                        <SlideUp delay={3}>
+                            <div className="flex gap-8 mt-10">
+                                <Button asChild size="lg" variant="pill" className="bg-transparent border-primary text-muted-foreground hover:text-cream-foreground hover:bg-primary">
+                                    <Link target="_blank" to={"https://docs.google.com/document/d/16dhelaYK01-PyEZFINDVS9rN9xXeCWte/edit?usp=sharing&ouid=105600997222878158182&rtpof=true&sd=true"}>Parents Handbook</Link>
+                                    
+                                </Button>
+                            </div>
+                            <div className="flex gap-8 mt-10">
+                                <Button asChild size="lg" variant="pill" className="bg-transparent border-primary text-muted-foreground hover:text-cream-foreground hover:bg-primary">
+                                    <Link target="_blank" to={"https://docs.google.com/document/d/1iepSpSbzfODN7wR3qiHPZbz1xnjzPQ5N/edit?usp=sharing&ouid=105600997222878158182&rtpof=true&sd=true"}>Program Information</Link>
+                                    
+                                </Button>
+                            </div>
+                        </SlideUp>
+                </div>
+                {/*<iframe class="google-container" src="https://docs.google.com/document/d/1WHbWLqe1T1dDS98xzZez_VTrNKLjXVOKYtURpjQ0zqA/preview?usp=sharing&embedded=true" 
                   width="100%" height="800px">
-                </iframe>
+                </iframe>*/}
                 {/* <Pagination /> */}
               </div>
               {/* <Sidebar /> */}
